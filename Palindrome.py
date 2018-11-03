18d2f6981e856f698111d5571dbfdb68400d65ad
Abhinav Kumar Raut <42289227+p11abhinavk@users.noreply.github.com>
2018-11-03 16:19:16 +0530

    Update Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,28 @@
+# function which return reverse of a string 
+def reverse(s): 
+	return s[::-1] 
+
+def isPalindrome(s): 
+	# Calling reverse function 
+	rev = reverse(s) 
+
+	# Checking if both string are equal or not 
+	if (s == rev): 
+		return True
+	return False
+
+
+# Driver code 
+s = "malayalam"
+ans = isPalindrome(s) 
+
+s = "data"
+ans = isPalindrome(s) 
+
+s = "dad"
+ans = isPalindrome(s)
+
+if ans == 1: 
+	print("Yes") 
+else: 
+	print("No") 
